fb7c1cfb3fef7e1e448dec4efb9fc0f3c10bd036
John Christensen <johnedchristensen@gmail.com>
2023-11-01 14:39:08 -0500

    built app

--- brick/main.py
+++ brick/main.py
@@ -10,10 +10,25 @@ class particleType(Enum):
     sand = 10
     water = 1
 
-myParticle = particleType.sand
 
-
-
+def findOpenWaterSpace(grid,x,y):
+    #check left
+    xl = x
+    while xl > 1:
+        xl -=1
+        if grid[xl][y] == particleType.empty:
+            return (xl,y)
+        if grid[xl][y] != particleType.water:
+            break
+    #check the right
+    xr = x
+    while xr  < WIDTH-1:
+        xr +=1
+        if grid[xr][y] == particleType.empty:
+            return (xr,y)
+        if grid[xr][y] != particleType.water:
+            break
+    return (x,y-1)
 
 class App:
     grid: list[list[particleType]] = [[]]
@@ -41,9 +56,9 @@ class App:
                         if y>0 and y < HEIGHT-1 and x >0 and x < WIDTH-1:
                             if self.grid[x][y+1] == particleType.sand:
                                 # Deterimine which way to fall
-                                if self.grid[x-1][y+1] == particleType.empty:
+                                if self.grid[x-1][y+1] != particleType.sand:
                                     newGrid[x-1][y+1] = particleType.sand
-                                elif self.grid[x+1][y+1] == particleType.empty:
+                                elif self.grid[x+1][y+1] != particleType.sand:
                                     newGrid[x+1][y+1] = particleType.sand
                                 else:
                                     newGrid[x][y] = particleType.sand
@@ -61,15 +76,11 @@ class App:
                                     newGrid[x+1][y+1] = particleType.water
                                 else:
                                     newGrid[x][y] = particleType.water
-                            if (self.grid[x][y-1] == particleType.water or
-                             self.grid[x+1][y-1] == particleType.water or
-                                self.grid[x-1][y-1] == particleType.water):
-                                if self.grid[x-1][y] ==particleType.empty:
-                                    newGrid[x-1][y] = particleType.water
-                                elif self.grid[x+1][y] ==particleType.empty:
-                                    newGrid[x+1][y] = particleType.water
-                                else:
-                                    newGrid[x][y] = particleType.water
+                            # If there is water above, try to let it flow into the current row
+                            if (self.grid[x][y-1] == particleType.water and self.grid[x][y-1] == particleType.water):
+                                water_x,water_y = findOpenWaterSpace(self.grid,x,y)
+                                newGrid[water_x][water_y] = particleType.water
+                                newGrid[x][y] = particleType.empty
                             else:
                                 newGrid[x][y+1] = particleType.water
                         else:
